bc83292b20341d41a984d770da13a8108b0bfd7c
Jonas Grunert <git@jonas-grunert.de>
2016-12-04 14:57:01 -0800

    Starting project

---
+++ ConcurrentGraph/concurrent-graph/src/main/java/mthesis/concurrent_graph/master/Master.java
@@ -0,0 +1,11 @@
+package mthesis.concurrent_graph.master;
+
+
+/**
+ * Concurrent graph processing master main
+ */
+public class Master {
+	public static void main(String[] args) {
+		System.out.println("Hello Master!");
+	}
+}
---
+++ ConcurrentGraph/concurrent-graph/src/main/java/mthesis/concurrent_graph/worker/Worker.java
@@ -0,0 +1,11 @@
+package mthesis.concurrent_graph.worker;
+
+
+/**
+ * Concurrent graph processing worker main
+ */
+public class Worker {
+	public static void main(String[] args) {
+		System.out.println("Hello Worker!");
+	}
+}
