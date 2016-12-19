809cb222080ddb145bcfc1531981300cff7ef8e8
Jonas Grunert <git@jonas-grunert.de>
2016-12-19 16:13:33 +0100

    Introducing configuration files

---
+++ ConcurrentGraph/concurrent-graph/src/main/java/mthesis/concurrent_graph/examples/common/MachineClusterConfiguration.java
@@ -0,0 +1,40 @@
+package mthesis.concurrent_graph.examples.common;
+
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import mthesis.concurrent_graph.MachineConfig;
+
+public class MachineClusterConfiguration {
+
+	public final int masterId;
+	public final List<Integer> AllWorkerIds = new ArrayList<>();
+	public final Map<Integer, MachineConfig> AllMachineConfigs = new HashMap<>();
+	public final Map<Integer, Boolean> StartOnThisMachine = new HashMap<>();
+
+	public MachineClusterConfiguration(String configFilePath) {
+		masterId = -1;
+		try {
+			List<String> lines = Files.readAllLines(Paths.get(configFilePath), Charset.forName("UTF-8"));
+			AddMachineConfig(-1, lines.get(1));
+			for (int i = 3; i < lines.size(); i++) {
+				AllWorkerIds.add(i - 3);
+				AddMachineConfig(i - 3, lines.get(i));
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void AddMachineConfig(int id, String cfg) {
+		String[] sSplit = cfg.split("\t");
+		AllMachineConfigs.put(id, new MachineConfig(sSplit[0], Integer.parseInt(sSplit[1])));
+		StartOnThisMachine.put(id, Boolean.parseBoolean(sSplit[2]));
+	}
+}
--- ConcurrentGraph/concurrent-graph/src/main/java/mthesis/concurrent_graph/examples/pagerank/PagerankTest.java
+++ ConcurrentGraph/concurrent-graph/src/main/java/mthesis/concurrent_graph/examples/pagerank/PagerankTest.java
@@ -1,12 +1,10 @@
 package mthesis.concurrent_graph.examples.pagerank;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import mthesis.concurrent_graph.MachineConfig;
 import mthesis.concurrent_graph.examples.common.ExampleTestUtils;
+import mthesis.concurrent_graph.examples.common.MachineClusterConfiguration;
 import mthesis.concurrent_graph.master.MasterOutputEvaluator;
 import mthesis.concurrent_graph.master.input.MasterInputPartitioner;
 import mthesis.concurrent_graph.master.input.RoundRobinBlockInputPartitioner;
@@ -17,34 +15,31 @@ import mthesis.concurrent_graph.writable.NullWritable;
 public class PagerankTest {
 
 	public static void main(String[] args) throws Exception {
-		final int numWorkers = 4;
-		final String host = "localhost";
-		final int baseControlMsgPort = 23499;
+		if (args.length < 3) {
+			System.out.println("Usage: [configFile] [inputFile] [partitionSize]");
+		}
+		MachineClusterConfiguration config = new MachineClusterConfiguration(args[0]);
+		final String inputFile = args[1];
+		final int partitionSize = Integer.parseInt(args[2]);
+
 		final String inputPartitionDir = "input";
 		final String outputDir = "output";
-		//final String inputFile = "../../Data_converted/cctest.txt";
-		final String inputFile = "../../../ConcurrentGraph_Data/converted/web-Stanford.txt";
-
 		final PagerankJobConfiguration jobConfig = new PagerankJobConfiguration();
-		//final MasterInputPartitioner inputPartitioner = new RoundRobinBlockInputPartitioner(1);
-		final MasterInputPartitioner inputPartitioner = new RoundRobinBlockInputPartitioner(10000);
+		// final MasterInputPartitioner inputPartitioner = new
+		// ContinousBlockInputPartitioner(partitionSize);
+		final MasterInputPartitioner inputPartitioner = new RoundRobinBlockInputPartitioner(partitionSize);
 		final MasterOutputEvaluator outputCombiner = new PagerankOutputEvaluator();
-
-		final Map<Integer, MachineConfig> allCfg = new HashMap<>();
-		final List<Integer> allWorkerIds= new ArrayList<>();
-		allCfg.put(-1, new MachineConfig(host, baseControlMsgPort));
-		for(int i = 0; i < numWorkers; i++) {
-			allWorkerIds.add(i);
-			allCfg.put(i, new MachineConfig(host, baseControlMsgPort + 1 + i));
-		}
 
 		System.out.println("Starting");
 		final ExampleTestUtils<DoubleWritable, NullWritable, DoubleWritable> testUtils = new ExampleTestUtils<>();
-		testUtils.startMaster(allCfg, -1, allWorkerIds, inputFile, inputPartitionDir, inputPartitioner, outputCombiner, outputDir);
+		if (config.StartOnThisMachine.get(config.masterId))
+			testUtils.startMaster(config.AllMachineConfigs, config.masterId, config.AllWorkerIds, inputFile,
+					inputPartitionDir, inputPartitioner, outputCombiner, outputDir);
 
 		final List<WorkerMachine<DoubleWritable, NullWritable, DoubleWritable>> workers = new ArrayList<>();
-		for(int i = 0; i < numWorkers; i++) {
-			workers.add(testUtils.startWorker(allCfg, i, allWorkerIds, outputDir, jobConfig));
+		for (int i = 0; i < config.AllWorkerIds.size(); i++) {
+			if (config.StartOnThisMachine.get(config.AllWorkerIds.get(i))) workers
+					.add(testUtils.startWorker(config.AllMachineConfigs, i, config.AllWorkerIds, outputDir, jobConfig));
 		}
 	}
 }
