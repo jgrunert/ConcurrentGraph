d29af7c75333252eae397a9c6589fb04c55581b2
Jonas Grunert <git@jonas-grunert.de>
2017-03-01 14:26:17 +0100

    Simple plotting tests

---
+++ ConcurrentGraph/concurrent-graph/src/main/java/mthesis/concurrent_graph/plotting/CsvDataFile.java
@@ -0,0 +1,64 @@
+package mthesis.concurrent_graph.plotting;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+public class CsvDataFile {
+
+	public final int NumDataColumns;
+	public final int NumDataRows;
+	public final String[] Captions;
+	public final double[][] Data;
+
+	public CsvDataFile(String file) throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		Captions = reader.readLine().split(";");
+		NumDataColumns = Captions.length;
+
+		List<double[]> dataLines = new ArrayList<>();
+		String line;
+		while ((line = reader.readLine()) != null) {
+			String[] lineSplit = line.split(";");
+			double[] dataLine = new double[NumDataColumns];
+			for (int i = 0; i < NumDataColumns; i++) {
+				dataLine[i] = Double.parseDouble(lineSplit[i]);
+			}
+			dataLines.add(dataLine);
+		}
+
+		NumDataRows = dataLines.size();
+		Data = new double[NumDataRows][];
+		for (int i = 0; i < NumDataRows; i++) {
+			Data[i] = dataLines.get(i);
+		}
+
+		reader.close();
+	}
+
+
+	/**
+	 * @return A JFreeChart dataset for this csv files with all columns. X value incremented per line.
+	 */
+	public XYDataset getColumnDatasets() {
+		final XYSeriesCollection dataset = new XYSeriesCollection();
+
+		for (int iCol = 0; iCol < NumDataColumns; iCol++) {
+			final XYSeries series = new XYSeries(Captions[iCol]);
+			for (int iRow = 0; iRow < NumDataRows; iRow++) {
+				series.add(iRow, Data[iRow][iCol]);
+				System.out.println(iRow + "," + Data[iRow][iCol]);
+			}
+			dataset.addSeries(series);
+		}
+
+		return dataset;
+
+	}
+}
--- ConcurrentGraph/concurrent-graph/src/main/java/mthesis/concurrent_graph/plotting/Plotter.java
+++ ConcurrentGraph/concurrent-graph/src/main/java/mthesis/concurrent_graph/plotting/Plotter.java
@@ -1,40 +1,71 @@
 package mthesis.concurrent_graph.plotting;
 
+import java.awt.Color;
 import java.io.File;
+import java.io.IOException;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartRenderingInfo;
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.entity.StandardEntityCollection;
-import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 
 public class Plotter {
 
 	public static void plotStats(String outputFolder) throws Exception {
-		//Prepare the data set
-		DefaultPieDataset pieDataset = new DefaultPieDataset();
-		pieDataset.setValue("Coca-Cola", 26);
-		pieDataset.setValue("Pepsi", 20);
-		pieDataset.setValue("Gold Spot", 12);
-		pieDataset.setValue("Slice", 14);
-		pieDataset.setValue("Appy Fizz", 18);
-		pieDataset.setValue("Limca", 10);
+		String statsFolder = outputFolder + File.separator + "stats";
+		CsvDataFile activeVerts = new CsvDataFile(statsFolder + File.separator + "0_times_ms.csv");
+		plotCsv(activeVerts, "test.png");
+	}
 
-		//Create the chart
-		JFreeChart chart = ChartFactory.createPieChart3D(
-				"Soft Dink 3D Pie Chart", pieDataset, true, true, true);
+	public static void plotCsv(CsvDataFile data, String outFile) throws IOException {
+		final JFreeChart chart = ChartFactory.createXYLineChart(
+				"Line Chart Demo 6", // chart title
+				"X", // x axis label
+				"Y", // y axis label
+				data.getColumnDatasets(), // data
+				PlotOrientation.VERTICAL,
+				true, // include legend
+				true, // tooltips
+				false // urls
+		);
 
-		//Save chart as PNG
+		// NOW DO SOME OPTIONAL CUSTOMISATION OF THE CHART...
+		chart.setBackgroundPaint(Color.white);
+
+		//	        final StandardLegend legend = (StandardLegend) chart.getLegend();
+		//      legend.setDisplaySeriesShapes(true);
+
+		// get a reference to the plot for further customisation...
+		final XYPlot plot = chart.getXYPlot();
+		plot.setBackgroundPaint(Color.lightGray);
+		//    plot.setAxisOffset(new Spacer(Spacer.ABSOLUTE, 5.0, 5.0, 5.0, 5.0));
+		plot.setDomainGridlinePaint(Color.white);
+		plot.setRangeGridlinePaint(Color.white);
+
+		final XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+		renderer.setSeriesLinesVisible(0, false);
+		renderer.setSeriesShapesVisible(1, false);
+		plot.setRenderer(renderer);
+
+		// change the auto tick unit selection to integer units only...
+		final NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+		rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+		// OPTIONAL CUSTOMISATION COMPLETED.
+
 		ChartRenderingInfo info = new ChartRenderingInfo(new StandardEntityCollection());
-		ChartUtilities.saveChartAsPNG(new File("soft3d.png"), chart, 400, 300, info);
+		ChartUtilities.saveChartAsPNG(new File(outFile), chart, 1200, 800, info);
 	}
 
 
 
 	public static void main(String[] args) {
 		try {
-			plotStats("");
+			plotStats("output");
 			System.out.println("Plot finished");
 		}
 		catch (Exception e) {
