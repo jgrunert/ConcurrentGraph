f0151dde17f822653dac232bef88660ed776a152
Jonas Grunert <git@jonas-grunert.de>
2017-03-31 17:35:47 +0200

    new short testsequence

--- ConcurrentGraph/concurrent-graph/src/main/java/mthesis/concurrent_graph/Configuration.java
+++ ConcurrentGraph/concurrent-graph/src/main/java/mthesis/concurrent_graph/Configuration.java
@@ -16,7 +16,7 @@ public class Configuration {
 	public static String ConfigFile;
 	public static Properties Properties = new Properties();
 
-	public static final String VERSION = "0.0.15";
+	public static final String VERSION = "0.1.0";
 
 	/** Maximum size of a message in bytes */
 	public static int MAX_MESSAGE_SIZE;
