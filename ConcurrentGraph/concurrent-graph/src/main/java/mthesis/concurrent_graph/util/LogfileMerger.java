1b515fa88a0277084505a653c3ab56fbe48134d0
Jonas Grunert <git@jonas-grunert.de>
2017-04-03 21:11:07 +0200

    Implemented LogfileMerger

---
+++ ConcurrentGraph/concurrent-graph/src/main/java/mthesis/concurrent_graph/util/LogfileMerger.java
@@ -0,0 +1,35 @@
+package mthesis.concurrent_graph.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LogfileMerger {
+
+	public static void main(String[] args) throws Exception {
+		String logfileDir = args[0];
+		System.out.println(logfileDir);
+
+		List<String> logLines = new ArrayList<>();
+		for (File logfile : new File(logfileDir).listFiles()) {
+			try (BufferedReader reader = new BufferedReader(new FileReader(logfile))) {
+				String line;
+				while ((line = reader.readLine()) != null) {
+					logLines.add(line);
+				}
+			}
+		}
+
+		Collections.sort(logLines);
+		try (PrintWriter writer = new PrintWriter(new FileWriter(logfileDir + File.separator + "log_merged.txt"))) {
+			for (String line : logLines) {
+				writer.println(line);
+			}
+		}
+	}
+}
