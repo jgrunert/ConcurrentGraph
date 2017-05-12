ec4041a16ab6278a2e3d4e02fd575a86a72f3b4e
Jonas Grunert <git@jonas-grunert.de>
2017-05-12 18:35:17 +0200

    WIP

--- concurrent-graph/src/main/java/mthesis/concurrent_graph/Configuration.java
+++ concurrent-graph/src/main/java/mthesis/concurrent_graph/Configuration.java
@@ -19,7 +19,7 @@ public class Configuration {
 	public static String ConfigFile;
 	public static Properties Properties = new Properties();
 
-	public static final String VERSION = "0.1.117";
+	public static final String VERSION = "0.1.118";
 
 	/** Maximum size of a message in bytes */
 	public static int MAX_MESSAGE_SIZE;
